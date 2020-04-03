719db4c94a63030a694569a8e88aff0b54548a25
Andrew Sernyak <andrew.sernyak@gmail.com>
2020-04-03 16:28:09 +0300

    * JS interop — way of defining workflow step/ctx function from JS

--- woof-playground/src/woof/browser.cljs
+++ woof-playground/src/woof/browser.cljs
@@ -16,6 +16,7 @@
 ;; or copy page contents into browser.html
 
 
+
 (enable-console-print!)
 
 
@@ -25,22 +26,37 @@
 ;; configure your browser wf here
 ;;
 
-(def init-fns   [])  ;; [scraper/scraper-init]
-(def ctx-fns    [dom/dom-ctx scraper2/ctx-fn])  ;; [dom/dom-ctx scraper/common-ctx scraper/scraper-ctx scraper/ws-ctx-fn]
-(def steps-fns  [scraper2/steps-fn])  ;; [scraper/scraper-steps]
-(def opt-fns    [scraper2/opt-fn])  ;; [scraper/common-opt]
+;(def init-fns   [])  ;; [scraper/scraper-init]
+;(def ctx-fns    [dom/dom-ctx scraper2/ctx-fn])  ;; [dom/dom-ctx scraper/common-ctx scraper/scraper-ctx scraper/ws-ctx-fn]
+;(def steps-fns  [scraper2/steps-fn])  ;; [scraper/scraper-steps]
+;(def opt-fns    [scraper2/opt-fn])  ;; [scraper/common-opt]
 
 
-(defn ^:export run_workflow []
+
+
+
+(defn convert [a]
+  (if (.isArray js/Array a)
+    (clj->js a)
+    a)
+  )
+
+(defn ^:export run_js_workflow [
+                                init-fns
+                                ctx-fns
+                                steps-fns
+                                opt-fns
+                                ]
   ;; this will start the wf
-  (let [wf-impl
+  (let [
+        wf-impl
         (base/parametrized-wf!
-          (base/combine-init-fns init-fns)
+          (base/combine-init-fns (convert init-fns))
           identity ; wf-params-fn
           identity ; opt-params-fn
-          (base/combine-fns opt-fns :merge-results base/merge-opts-maps)
-          (base/combine-fns ctx-fns)
-          (base/combine-fns steps-fns))
+          (base/combine-fns (convert opt-fns) :merge-results base/merge-opts-maps)
+          (base/combine-fns (convert ctx-fns))
+          (base/combine-fns (convert steps-fns)))
         ]
     (base/run-wf! wf-impl identity)
     )
@@ -48,9 +64,121 @@
 
 
 
+(defn ^:export run_workflow []
+
+  (run_js_workflow
+    []
+    [dom/dom-ctx scraper2/ctx-fn]
+    [scraper2/steps-fn]
+    [scraper2/opt-fn]
+    )
+  )
+
+
+
+
+(defn ^:export default_ctx_fn [params]
+  {
+   :identity {:fn identity}
+   }
+  )
+
+(defn ^:export default_opt_fn [params]
+  {
+   :op-handlers-map {
+                     :done  (fn [result]
+                              (.groupCollapsed js/console "Workflow ended.")
+                              (.log js/console
+                                    "Full results map: " result)
+                              (.groupEnd js/console)
+                              )
+
+                     :error (fn [result]
+                              (.error js/console result))
+
+                     }
+
+   })
+
+
+(defn ^:export k [s]
+  (keyword s))
+
+
+(defn ^:export qk
+  ([k]
+   (let [s (str k)
+         [raw-ns n] (clojure.string/split s #"/")
+         ns (if (clojure.string/starts-with? raw-ns ":")
+              (if (clojure.string/starts-with? raw-ns "::")
+                (subs raw-ns 2)
+                (subs raw-ns 1))
+              raw-ns)
+         ]
+     (if (nil? n)
+       (keyword (namespace ::boo) ns)
+       (keyword ns n)
+       )
+     )
+   )
+  ([ns k]
+   (keyword ns k)
+   ))
+
+(defn ^:export kv []
+  {})
+
+
+
+;; wraps js function into a step fn
+(defn ^:export steps_fn [js-fn]
+  (fn [params]
+    (let [js-res (js-fn (clj->js params))]
+
+         ;; convert js result to
+         (into {} (for [k (.keys js/Object js-res)]
+                    (do
+                      ;(js-debugger)
+
+                      (let [nu-k (if (qualified-keyword? k) k (qk k))
+                            raw-v (aget js-res k)
+                            raw-step (aget raw-v 0)
+                            step (if (keyword? raw-step) raw-step (k raw-step))
+                            v (aget raw-v 1)
+                            ]
+                        [nu-k [step v]]
+                        )
+
+                      )
+
+                    ))
+         )))
+
+
+(defn ^:export ctx_fn [js-fn]
+  (fn [params]
+    (let [js-res (js-fn (clj->js params))]
+
+         ;; convert js result to
+         (into {} (for [k (.keys js/Object js-res)]
+                    (do
+                      (let [nu-k (if (keyword? k) k (keyword k))
+                            raw-cfg (aget js-res k)
+                            cfg (js->clj raw-cfg :keywordize-keys true)
+                            ]
+                        (.log js/console cfg)
+                        [nu-k cfg]
+                        )
+
+                      )
+
+                    ))
+         )))
+
+
+
 ;; for now - auto-run wf, if we are not in the playground
 (when-not (goog.object/get js/window "PLAYGROUND")
-
   ; (.clear js/console)
   (run_workflow)
 
