bc9b3fad1dc7ace50e1dce1165758250a04cc658
Andrew Sernyak <ndrew.sernyak@gmail.com>
2020-09-03 16:39:39 +0300

    Give voice signal if scraper workflow had been compiled or not. This is needed to know for sure when to refresh chrome extension without looking to console

--- woof-playground/scripts/build-scraper-wf.sh
+++ woof-playground/scripts/build-scraper-wf.sh
@@ -9,9 +9,17 @@ echo "START: scraper.js";
 # compile cljs to resources/public/scraper/scraper.js
 cd ~/m/woof/woof-playground/
 
-clj -m cljs.main --optimizations whitespace --compile-opts scripts/scraper.edn --output-to  ~/m/woof/woof-playground/resources/public/scraper/scraper.js -c woof.browser
 
-# copy it to chrome extension
-cp ~/m/woof/woof-playground/resources/public/scraper/scraper.js ~/m/woof/woof-chrome/scraper.js
+OUTPUT=$( clj -m cljs.main --optimizations whitespace --compile-opts scripts/scraper.edn --output-to  ~/m/woof/woof-playground/resources/public/scraper/scraper.js -c woof.browser 2>&1 );
 
-echo "DONE:  scraper.js";
+if [ -z "$OUTPUT" ]; then
+  # copy it to chrome extension
+  cp ~/m/woof/woof-playground/resources/public/scraper/scraper.js ~/m/woof/woof-chrome/scraper.js
+
+  echo "DONE:  scraper.js";
+  say -v Victoria Build completed;
+else
+  echo "CAN'T COMPILE scraper.js";
+  say -v Victoria Error;
+fi
+
