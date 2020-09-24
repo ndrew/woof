4c7555a0209ff8f2de32f5ab6ec4f5d78c16287c
Andrew Sernyak <ndrew.sernyak@gmail.com>
2020-09-24 18:09:43 +0300

    scraping workflow + playground - compile both to same file, so I can work both on scraping workflow and analyzing results in playground

---
+++ woof-playground/src/woof/scraper.cljs
@@ -0,0 +1,7 @@
+(ns woof.scraper
+  (:require
+    [woof.playground :as playground]
+    [woof.browser :as browser]
+    ))
+
+;; reference browser and playground so both scraping workflows and playground will be compiled
