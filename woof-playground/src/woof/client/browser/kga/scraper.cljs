99149a2e8fb0b838d28841935f75a7fc0621e697
Andrew Sernyak <ndrew.sernyak@gmail.com>
2020-10-27 17:31:23 +0200

    WIP: kga scraping - linearized fetching

--- woof-playground/src/woof/client/browser/kga/scraper.cljs
+++ woof-playground/src/woof/client/browser/kga/scraper.cljs
@@ -23,7 +23,117 @@
 
     [cljs.core.async :refer [go] :as async]
     [cljs.core.async.interop :refer-macros [<p!]]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+
+    [woof.wfs.alpha :as alpha]
+    ))
+
+
+(defonce SEQ-ID ::fetcher)
+
+
+(defonce STREET-IDS
+         [10013 10023 10034 10035 10042 10043 10044 10049 10050 10055 10068 10069 10072 10073 10074 10084 10085 10089 10090 10104 10109 10144 10149 10160 10161 10186 10190 10201 10228 10277 10283 10286 10312 10325 10326 10330 10331 10332 10339 10351 10352 10354 10382 10388 10392 10413 10418 10427 10428 10440 10461 10462 10473 10474 10510 10511 10517 10521 10525 10593 10594 10596 10599 10619 10623 10635 10670 10678 10679 10684 10702 10748 10765 10767 10773 10777 10778 10786 10799 10812 10830 10831 10836 10864 10885 10891 10903 10956 10957 10958 10976 10977 10981 10988 10991 11001 11037 11043 11060 11061 11062 11081 11110 11112 11124 11125 11134 11135 11136 11138 11140 11141 11162 11174 11180 11182 11183 11195 11196 11202 11204 11208 11209 11233 11252 11259 11264 11267 11268 11271 11277 11280 11281 11282 11293 11297 11313 11315 11326 11328 11334 11335 11349 11381 11390 11398 11426 11433 11438 11442 11446 11467 11471 11495 11496 11497 11505 11543 11561 11566 11577 11578 11586 11587 11596 11597 11604 11608 11609 11610 11611 11626 11628 11631 11651 11654 11655 11657 11661 11672 11676 11679 11690 11691 11695 11704 11705 11708 11712 11723 11732 11743 11744 11774 11795 11807 11824 11825 11835 11837 11838 11858 11863 11868 11878 11885 11886 11887 11889 11894 11896 11901 11907 11911 11912 11915 11921 11922 11927 11928 11943 11957 11958 11966 11986 11996 12023 12030 12059 12110 12609 12731 13119]
+         )
+
+
+
+(defn wf-ctx [params]
+  (let [cf (base/&chan-factory params)
+        fetch-opts (js-obj
+                     "headers" (js-obj
+                                 "accept" "*/*"
+                                 "accept-language" "en-US,en;q=0.9,ru;q=0.8"
+                                 "sec-fetch-dest" "empty"
+                                 "sec-fetch-mode" "cors"
+                                 "sec-fetch-site" "same-origin"
+                                 )
+                     "referrer" "https://mkk.kga.gov.ua/map/"
+                     "referrerPolicy" "strict-origin-when-cross-origin"
+                     "body" nil,
+                     "method" "GET"
+                     "mode" "cors"
+                     "credentials" "include"
+                     )
+
+        fetch-fn (fn [id]
+                   (let [chan (base/make-chan cf (base/rand-sid))
+
+
+                         ;url (str "https://mkk.kga.gov.ua/arcgis/rest/services/Search/AddressSearch/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=0.29858214164761665&objectIds="
+                         ;         id "&outFields=*&outSR=102100")
+
+                         url (str "https://mkk.kga.gov.ua/arcgis/rest/services/Search/AddressSearch/MapServer/0/query"
+                                  "?f=json&where=StrCodeObj" "%3D" id "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100")
+                         ]
+
+                     (go
+                       (let [req (<p! (js/fetch url fetch-opts))
+                             json (<p! (.json req))
+
+                             details (js->clj json :keywordize-keys true)]
+
+                         ;; in order to process full response
+                         ; (async/put! chan details)
+
+                         ;; extract house nums
+                         (let [features (get details :features [])
+                               houses (into #{} (map #(get-in % [:attributes :AddrNumb1]) features))]
+
+                           (async/put! chan {id houses})
+                           )
+
+                         ;;
+                         (async/put! chan details)))
+                     chan
+                     )
+                   )
+        ]
+
+    {
+
+     :merge        {
+                    :fn       (fn [vs]
+                                (apply merge vs)
+                                )
+                    :collect? true
+                    }
+
+     :save-edn     {
+                    :fn (fn [edn]
+                          (woof-dom/save-edn (str "kga-houses-" (u/now) ".edn") edn)
+                          :ok)
+
+                    }
+
+     :custom-log   {:fn (fn [v]
+                          (.groupCollapsed js/console "DETAILS")
+                          (.log js/console (d/pretty! v))
+                          (.groupEnd js/console)
+                          )}
+
+     ;; normal expand
+     :fetch-houses {:fn fetch-fn}
+     :process*     (base/expand-into :fetch-houses)
+
+
+     ;; linearized fetch
+     :seq-process* {:fn       (partial alpha/_seq-worker-expander
+                                       SEQ-ID
+                                       (fn [id]
+                                         (.log js/console ":process*\tid=" id)
+                                         (fetch-fn id)
+                                         )
+                                       params
+                                       )
+                    :expands? true
+                    }
+
+     }
+    )
+  )
+
+
 
 
 (defn clean-css-init [params]
@@ -49,334 +159,21 @@
    })
 
 
-
-
-(defn wf-ctx [params]
-  (let [cf (base/&chan-factory params)
-        fetch-opts (js-obj
-                     "headers" (js-obj
-                                 "accept" "*/*"
-                                 "accept-language" "en-US,en;q=0.9,ru;q=0.8"
-                                 "sec-fetch-dest" "empty"
-                                 "sec-fetch-mode" "cors"
-                                 "sec-fetch-site" "same-origin"
-                                 )
-                     "referrer" "https://mkk.kga.gov.ua/map/"
-                     "referrerPolicy" "strict-origin-when-cross-origin"
-                     "body" nil,
-                     "method" "GET"
-                     "mode" "cors"
-                     "credentials" "include"
-                     )]
-
-    {
-
-     :fetch-houses    {:fn (fn [id]
-                              (let [chan (base/make-chan cf (base/rand-sid))
-
-
-                                    ;url (str "https://mkk.kga.gov.ua/arcgis/rest/services/Search/AddressSearch/MapServer/0/query?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&maxAllowableOffset=0.29858214164761665&objectIds="
-                                    ;         id "&outFields=*&outSR=102100")
-
-                                    url (str "https://mkk.kga.gov.ua/arcgis/rest/services/Search/AddressSearch/MapServer/0/query"
-                                             "?f=json&where=StrCodeObj" "%3D" id "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100")
-                                    ]
-
-                                (go
-                                  (let [req (<p! (js/fetch url fetch-opts))
-                                        json (<p! (.json req))
-
-                                        details (js->clj json :keywordize-keys true)]
-
-                                    ;; in order to process full response
-                                    ; (async/put! chan details)
-
-                                    ;; extract house nums
-                                    (let [features (get details :features [])
-                                          houses (into #{} (map #(get-in % [:attributes :AddrNumb1]) features))]
-
-                                      (async/put! chan {id houses})
-                                      )
-
-                                    ;;
-                                    (async/put! chan details)))
-                                chan
-                                )
-                              )
-                        }
-
-     :process*         (base/expand-into :fetch-houses)
-
-     :merge            {
-                        :fn       (fn [vs]
-                                    (apply merge vs)
-                                    )
-                        :collect? true
-                        }
-
-     :save-edn {
-                :fn (fn [edn]
-                      (woof-dom/save-edn (str "kga-houses-" (u/now) ".edn") edn)
-                      :ok)
-
-                }
-
-     :custom-log {:fn (fn [v]
-                        (.groupCollapsed js/console "DETAILS")
-                        (.log js/console (d/pretty! v))
-                        (.groupEnd js/console)
-                        )}
-
-     }
-    )
-  )
-
-
-
-(defonce STREET-IDS
-         [10020
-          10021
-          10033
-          10086
-          10087
-          10093
-          10099
-          10100
-          10112
-          10118
-          10123
-          10124
-          10128
-          10131
-          10132
-          10147
-          10150
-          10157
-          10177
-          10189
-          10200
-          10209
-          10210
-          10213
-          10215
-          10225
-          10258
-          10259
-          10261
-          10263
-          10279
-          10302
-          10334
-          10348
-          10349
-          10350
-          10353
-          10364
-          10383
-          10405
-          10414
-          10429
-          10439
-          10475
-          10476
-          10498
-          10499
-          10518
-          10527
-          10542
-          10559
-          10560
-          10562
-          10572
-          10582
-          10586
-          10597
-          10598
-          10602
-          10603
-          10607
-          10610
-          10625
-          10646
-          10650
-          10668
-          10675
-          10704
-          10705
-          10712
-          10745
-          10747
-          10750
-          10761
-          10769
-          10795
-          10797
-          10798
-          10893
-          10910
-          10937
-          10938
-          10944
-          10945
-          10973
-          11032
-          11033
-          11034
-          11051
-          11068
-          11071
-          11079
-          11087
-          11088
-          11099
-          11118
-          11120
-          11121
-          11133
-          11144
-          11147
-          11164
-          11175
-          11187
-          11188
-          11199
-          11210
-          11212
-          11234
-          11245
-          11247
-          11251
-          11262
-          11274
-          11275
-          11289
-          11299
-          11300
-          11314
-          11318
-          11324
-          11325
-          11342
-          11343
-          11351
-          11352
-          11353
-          11369
-          11399
-          11400
-          11440
-          11441
-          11453
-          11454
-          11455
-          11463
-          11476
-          11480
-          11490
-          11491
-          11492
-          11510
-          11512
-          11542
-          11552
-          11589
-          11590
-          11605
-          11606
-          11640
-          11641
-          11642
-          11645
-          11646
-          11674
-          11681
-          11716
-          11724
-          11728
-          11777
-          11778
-          11781
-          11798
-          11799
-          11818
-          11823
-          11852
-          11869
-          11870
-          11914
-          11919
-          11934
-          11936
-          11937
-          11959
-          11960
-          11970
-          11971
-          11977
-          11997
-          12000
-          12001
-          12006
-          12010
-          12011
-          12019
-          12028
-          12033
-          12043
-          12046
-          12050
-          12054
-          12462
-          12463
-          12464
-          12465
-          12466
-          12467
-          12468
-          12469
-          12470
-          12471
-          12472
-          12473
-          12474
-          12475
-          12476
-          12477
-          12478
-          12479
-          12480
-          12481
-          12482
-          12483
-          12484
-          12485
-          12486
-          12487
-          12488
-          12489
-          12490
-          12491
-          12610
-          12702
-          12709
-          12913
-          12915
-          12918
-          12919
-          12920
-          12922
-          13098
-          13099]
-         )
-
 (defn wf! [*wf-state meta-info]
 
   {
-   :init    [clean-css-init]
+   :init    [clean-css-init
+             ;; provide linearization for doing expands
+             (fn [params]
+               (alpha/_seq-worker-init SEQ-ID params))
+             ]
 
    :ctx     [; watcher/watcher-ctx
 
              wf-ctx
+
+
+
              ]
 
    :steps   [
@@ -385,7 +182,10 @@
 
                 :street/ids [:v STREET-IDS]
 
-                :xtract/street-house-kv [:process* :street/ids]
+                ;; all expand
+                ; :xtract/street-house-kv [:process* :street/ids]
+                ;; linearized process
+                :xtract/street-house-kv [:seq-process* :street/ids]
                 :xtract/street-house-map [:merge :xtract/street-house-kv]
 
                 :log/streets [:&log :xtract/street-house-map]
