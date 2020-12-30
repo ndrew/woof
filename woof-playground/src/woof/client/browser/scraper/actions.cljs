7770dbbdbac964f9200f72aebabfbca11848baa3
Andrew Sernyak <ndrew.sernyak@gmail.com>
2020-12-30 15:36:51 +0200

    scraper sub-workflows * rum based ui * keyboard triggered wf actions

---
+++ woof-playground/src/woof/client/browser/scraper/actions.cljs
@@ -0,0 +1,50 @@
+(ns woof.client.browser.scraper.actions
+  (:require
+    [woof.base :as base]
+    [woof.client.dom :as woof-dom]
+    ))
+
+
+;; (woof-dom/chord 83 :shift true :meta true)
+(defn chord-action [chord action-name f ]
+  [action-name
+   (with-meta f {:shortcut chord} )
+   ;; #^{:shortcut chord} f
+   ])
+
+
+(defn actions-impl! [API on-chord]
+
+  (let [SHORTCUTS (reduce (fn [a [k f]]
+                            (if f
+                              (if-let [m (meta f)]
+                                (if (contains? m :shortcut)
+                                  (assoc a (:shortcut m) f))
+                                a
+                                )
+                              a
+                              )
+                            ) {} API)
+
+        klog (fn [e]
+               (let [chord (into {} (for [[key attr] {:shift "shiftKey" :ctrl "ctrlKey" :alt "altKey" :meta "metaKey" :code  "keyCode"}]
+                                      [key (aget e attr)]))]
+
+                 (on-chord chord SHORTCUTS)
+
+                 )
+               )
+        ]
+    {
+     :init [(fn [params]
+              (js/addEventListener "keydown" klog false)
+              {}
+              )]
+     :opts [(base/build-opt-on-done (fn [_ _]
+                                      ; (.warn js/console result)
+                                      (js/removeEventListener "keydown" klog false)))
+            ]
+     }
+    )
+
+  )
---
+++ woof-playground/src/woof/client/browser/scraper/rum_ui.cljs
@@ -0,0 +1,72 @@
+(ns woof.client.browser.scraper.rum-ui
+  (:require
+    [goog.dom :as dom]
+
+    [rum.core :as rum]
+
+    [woof.base :as base]
+    [woof.client.dom :as woof-dom]
+    [woof.wfs.watcher :as watcher]
+
+    ))
+
+;;
+;; wrapper to add rum ui to a scraping workflow
+;;
+
+(defn gen-rum-ui [<scraping-ui>]
+  ;; ndrw: should separate arity be used for UI mount?
+  (fn
+    ([]
+     ;; init UI
+     (let [$panel (dom/createDom "div" "panel woof-scrape-panel ui-container")]
+       (woof-dom/ui-add-el! $panel))
+     )
+    ([*state STATE]
+     (if-let [container-el (woof-dom/q ".ui-container")]
+       ;; react UI
+       (rum/mount (<scraping-ui> *state STATE) container-el)
+       (let [$panel (dom/createDom "div" "panel woof-scrape-panel ui-container")]
+         (woof-dom/ui-add-el! $panel)
+         (rum/mount (<scraping-ui> *state STATE) $panel))
+       )))
+  )
+
+
+
+
+(defn ui-impl! [*WF-UI <rum-ui>]
+  (let [WATCHER-ID (base/rand-sid "watcher-")]
+
+    ;; A) SIDE-EFFECTS
+    {
+     :init [
+            (fn [params]
+              ;; B) SIDE-EFFECTS
+
+              (merge
+                {
+                 :wf/*UI *WF-UI
+                 }
+                (watcher/_watcher-cf-init-cb
+                  WATCHER-ID *WF-UI
+                  (fn [*state state]
+                    ;; (.log js/console "UI: upd" state (= state @*state))
+                    (<rum-ui> *state state))
+                  params)))]
+
+     :ctx  [watcher/watcher-ctx]
+     :opts [
+            ;; C) SIDE-EFFECTS
+            ;;
+            #_{:before-process (fn [_ _]
+                                 ; (<rum-ui>)
+
+                                 :ok
+                                 )}
+            watcher/watcher-opts]
+     }
+    )
+  )
+
+
